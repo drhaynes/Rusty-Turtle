7378f738766edae3ca416d297572b1de4e3713db
David Haynes <drhmbp@gmail.com>
2021-12-18 13:45:30 +0000

    Add initial parser datatypes

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,3 +1,17 @@
+enum ParseError {
+    GeneralError,
+}
+
+enum AST {
+    Token,
+}
+
+trait Parser {
+    fn parse(string: &str) -> Result<AST, ParseError> {
+        unimplemented!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
